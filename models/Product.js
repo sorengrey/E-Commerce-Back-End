33676919c91a5ad934ec92f8f506194520bb5a1f
Heather Smith <halexsmith86@gmail.com>
2021-03-17 16:28:57 -0400

    Filled out columns in models - need references now

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }, 
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // validation for whether or not the value is a decimal
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      default: 10,
+      // validation for whether or not the value is a number
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // reference to Category's model id #
+    },
   },
   {
     sequelize,
